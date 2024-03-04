7c8a85cbb72b3e3b48b5f58e6e947e1b1bcb2f2e
asbjornfa <asbjornfa@gmail.com>
2024-03-04 22:26:54 +0100

    En lille start til klassen

--- src/dk/easv/bll/bot/Aiqaeda.java
+++ src/dk/easv/bll/bot/Aiqaeda.java
@@ -2,8 +2,22 @@ package dk.easv.bll.bot;
 
 import dk.easv.bll.game.IGameState;
 import dk.easv.bll.move.IMove;
+import javafx.scene.Node;
+
+import java.util.List;
+import java.util.Random;
 
 public class Aiqaeda implements IBot {
+
+    private int previousAction; // the move that led to this state
+    private Node parent; // parent node
+    private List<Node> children; // child nodes
+    private int wins; // number of wins
+    private int visits; // number of visits
+    private List<Integer> untriedActions; // actions not yet explored
+    private int previousPlayer; // the player who made the previous move
+    private String BOT_NAME = getClass().getSimpleName();
+    private Random random = new Random();
 
     @Override
     public IMove doMove(IGameState state) {
@@ -12,6 +26,6 @@ public class Aiqaeda implements IBot {
 
     @Override
     public String getBotName() {
-        return null;
+        return BOT_NAME;
     }
 }
