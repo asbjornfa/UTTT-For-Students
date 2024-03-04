f1e076a7b71933643ac8ae0b3f0ff436e8c2ba55
asbjornfa <asbjornfa@gmail.com>
2024-03-04 11:00:55 +0100

    bot class

--- src/dk/easv/bll/bot/Aiqaeda.java
+++ src/dk/easv/bll/bot/Aiqaeda.java
@@ -1,4 +1,17 @@
 package dk.easv.bll.bot;
 
-public class Aiqaeda {
+import dk.easv.bll.game.IGameState;
+import dk.easv.bll.move.IMove;
+
+public class Aiqaeda implements IBot {
+
+    @Override
+    public IMove doMove(IGameState state) {
+        return null;
+    }
+
+    @Override
+    public String getBotName() {
+        return null;
+    }
 }
