3ed22af50d2edec96f7c144d9abfdc828a6641c1
nikolaj1307 <nikolaj1307@live.dk>
2024-03-07 12:29:20 +0100

    AiQaeda improvement

--- src/dk/easv/bll/bot/AiQaeda.java
+++ src/dk/easv/bll/bot/AiQaeda.java
@@ -4,52 +4,134 @@ import dk.easv.bll.field.IField;
 import dk.easv.bll.game.GameState;
 import dk.easv.bll.game.IGameState;
 import dk.easv.bll.move.IMove;
+import dk.easv.bll.move.Move;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.Random;
 
 public class AiQaeda implements IBot {
 
     private Random rand = new Random();
-    final  int moveTimeMs = 1000;
-    private String BOT_NAME = getClass().getSimpleName();
+    private final int moveTimeMs = 1000;
+    private final String BOT_NAME = getClass().getSimpleName();
 
-    private GameSimulator createSimulator(IGameState state) {
-        GameSimulator simulator = new GameSimulator(new GameState());
-        simulator.setGameOver(GameOverState.Active);
-        simulator.setCurrentPlayer(state.getMoveNumber() % 2);
-        simulator.getCurrentState().setRoundNumber(state.getRoundNumber());
-        simulator.getCurrentState().setMoveNumber(state.getMoveNumber());
-        simulator.getCurrentState().getField().setBoard(state.getField().getBoard());
-        simulator.getCurrentState().getField().setMacroboard(state.getField().getMacroboard());
-        return simulator;
-    }
+    private static final int BOARD_SIZE = 9;
+    private static final int SUBGRID_SIZE = 3;
 
     @Override
     public IMove doMove(IGameState state) {
         return calculateMove(state, moveTimeMs);
     }
 
+    /**
+     * Calculates the next move based on the current game state.
+     *
+     * @param state      The current game state.
+     * @param maxTimeMs  The maximum time allowed for the AI to make a move.
+     * @return The calculated move.
+     */
     private IMove calculateMove(IGameState state, int maxTimeMs) {
-        long time = System.currentTimeMillis();
-        while (System.currentTimeMillis() < time + maxTimeMs) {
-            GameSimulator simulator = createSimulator(state);
-            List<IMove> legalMoves = simulator.getCurrentState().getField().getAvailableMoves();
+        // Create a simulator to simulate the current state of the game
+        GameSimulator simulator = createSimulator(state);
 
-            // Strategies
+        // Get a list of legal moves available in the current state
+        List<IMove> legalMoves = simulator.getCurrentState().getField().getAvailableMoves();
 
-            // Should start by putting the first piece in the middle of the macroboard and the middle of the microboard.
-            for (IMove move : legalMoves) {
-                if (move.getX() == 4 && move.getY() == 4) {
-                    return move; // Place the first piece in the middle of the macroboard and the microboard
-                }
+        // Check for winning move and return if found
+        IMove winningMove = evaluateMoves(legalMoves, simulator, false);
+        if (winningMove != null) return winningMove;
+
+        // Check for blocking move and return if found
+        IMove blockingMove = evaluateMoves(legalMoves, simulator, true);
+        if (blockingMove != null) return blockingMove;
+
+        // Optimize the initial move if it's the first move of the game
+        if (state.getMoveNumber() == 0) {
+            return optimizeInitialMove(legalMoves);
+        }
+
+        // Find a defensive move to block opponent's potential winning move
+        IMove defensiveMove = findDefensiveMove(legalMoves, simulator);
+        return (defensiveMove != null) ? defensiveMove : legalMoves.get(rand.nextInt(legalMoves.size()));
+    }
+
+    /**
+     * Finds a defensive move to block the opponent from winning in the next turn.
+     *
+     * @param legalMoves List of legal moves in the current state.
+     * @param simulator  The game simulator.
+     * @return The defensive move if found, otherwise null.
+     */
+    private IMove findDefensiveMove(List<IMove> legalMoves, GameSimulator simulator) {
+        for (IMove move : legalMoves) {
+            // Create a simulator for evaluating opponent's moves
+            GameSimulator evalSimulator = createSimulator(simulator.getCurrentState());
+            evalSimulator.setCurrentPlayer((simulator.getCurrentPlayer() + 1) % 2);
+            evalSimulator.updateGame(move);
+
+            // Get opponent's legal moves
+            List<IMove> opponentMoves = evalSimulator.getCurrentState().getField().getAvailableMoves();
+
+            // Check if opponent has a winning move
+            if (evaluateMoves(opponentMoves, evalSimulator, false) != null) {
+                return move;
             }
-
-            // If no strategic moves are found, make a random move
-            return legalMoves.get(rand.nextInt(legalMoves.size()));
         }
         return null;
+    }
+
+    /**
+     * Optimizes the initial move by choosing a corner if available, otherwise, chooses a random move.
+     *
+     * @param legalMoves List of legal moves in the current state.
+     * @return The optimized initial move.
+     */
+    private IMove optimizeInitialMove(List<IMove> legalMoves) {
+        for (IMove move : legalMoves) {
+            // Choose a corner move if available
+            if ((move.getX() % 2 == 0) && (move.getY() % 2 == 0)) {
+                return move;
+            }
+        }
+        // If no corner found, choose a random move
+        return legalMoves.get(rand.nextInt(legalMoves.size()));
+    }
+
+    /**
+     * Evaluates moves based on the specified criteria (winning or blocking).
+     *
+     * @param moves     List of possible moves.
+     * @param simulator The game simulator.
+     * @param blocking  Flag indicating if the evaluation is for blocking.
+     * @return The evaluated move if found, otherwise null.
+     */
+    private IMove evaluateMoves(List<IMove> moves, GameSimulator simulator, boolean blocking) {
+        for (IMove move : moves) {
+            // Create a simulator for evaluating moves
+            GameSimulator evalSimulator = createSimulator(simulator.getCurrentState());
+            evalSimulator.setCurrentPlayer(blocking ? (simulator.getCurrentPlayer() + 1) % 2 : simulator.getCurrentPlayer());
+            evalSimulator.updateGame(move);
+
+            // Check if the move results in a win
+            if (evalSimulator.getGameOver() == GameOverState.Win) {
+                return move;
+            }
+        }
+        return null;
+    }
+
+    private GameSimulator createSimulator(IGameState state) {
+        GameSimulator simulator = new GameSimulator(new GameState());
+        simulator.setGameOver(GameOverState.Active);
+        simulator.setCurrentPlayer(state.getMoveNumber() % 2);
+
+        simulator.getCurrentState().setRoundNumber(state.getRoundNumber());
+        simulator.getCurrentState().setMoveNumber(state.getMoveNumber());
+
+        simulator.getCurrentState().getField().setBoard(state.getField().getBoard());
+        simulator.getCurrentState().getField().setMacroboard(state.getField().getMacroboard());
+        return simulator;
     }
 
     @Override
@@ -63,48 +145,14 @@ public class AiQaeda implements IBot {
         Tie
     }
 
-    public class Move implements IMove {
-        int x = 0;
-        int y = 0;
-
-        public Move(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public int getX() {
-            return x;
-        }
-
-        @Override
-        public int getY() {
-            return y;
-        }
-
-        @Override
-        public String toString() {
-            return "(" + x + "," + y + ")";
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Move move = (Move) o;
-            return x == move.x && y == move.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
-    }
-
     class GameSimulator {
         private final IGameState currentState;
-        private int currentPlayer = 0; //player0 == 0 && player1 == 1
+        private int currentPlayer = 0;
         private volatile GameOverState gameOver = GameOverState.Active;
+
+        public GameSimulator(IGameState currentState) {
+            this.currentState = currentState;
+        }
 
         public void setGameOver(GameOverState state) {
             gameOver = state;
@@ -114,16 +162,20 @@ public class AiQaeda implements IBot {
             return gameOver;
         }
 
+        public int getCurrentPlayer() {
+            return currentPlayer;
+        }
+
         public void setCurrentPlayer(int player) {
             currentPlayer = player;
         }
 
-        public IGameState getCurrentState() {
-            return currentState;
+        public void switchPlayer() {
+            currentPlayer = (currentPlayer + 1) % 2;
         }
 
-        public GameSimulator(IGameState currentState) {
-            this.currentState = currentState;
+        public IGameState getCurrentState() {
+            return currentState;
         }
 
         public Boolean updateGame(IMove move) {
@@ -131,7 +183,7 @@ public class AiQaeda implements IBot {
                 return false;
 
             updateBoard(move);
-            currentPlayer = (currentPlayer + 1) % 2;
+            switchPlayer();
 
             return true;
         }
@@ -140,8 +192,8 @@ public class AiQaeda implements IBot {
             IField field = currentState.getField();
             boolean isValid = field.isInActiveMicroboard(move.getX(), move.getY());
 
-            if (isValid && (move.getX() < 0 || 9 <= move.getX())) isValid = false;
-            if (isValid && (move.getY() < 0 || 9 <= move.getY())) isValid = false;
+            if (isValid && (move.getX() < 0 || BOARD_SIZE <= move.getX())) isValid = false;
+            if (isValid && (move.getY() < 0 || BOARD_SIZE <= move.getY())) isValid = false;
 
             if (isValid && !field.getBoard()[move.getX()][move.getY()].equals(IField.EMPTY_FIELD))
                 isValid = false;
@@ -153,18 +205,18 @@ public class AiQaeda implements IBot {
             String[][] board = currentState.getField().getBoard();
             board[move.getX()][move.getY()] = currentPlayer + "";
             currentState.setMoveNumber(currentState.getMoveNumber() + 1);
+
             if (currentState.getMoveNumber() % 2 == 0) {
                 currentState.setRoundNumber(currentState.getRoundNumber() + 1);
             }
             checkAndUpdateIfWin(move);
             updateMacroboard(move);
-
         }
 
         private void checkAndUpdateIfWin(IMove move) {
             String[][] macroBoard = currentState.getField().getMacroboard();
-            int macroX = move.getX() / 3;
-            int macroY = move.getY() / 3;
+            int macroX = move.getX() / SUBGRID_SIZE;
+            int macroY = move.getY() / SUBGRID_SIZE;
 
             if (macroBoard[macroX][macroY].equals(IField.EMPTY_FIELD) ||
                     macroBoard[macroX][macroY].equals(IField.AVAILABLE_FIELD)) {
@@ -176,23 +228,21 @@ public class AiQaeda implements IBot {
                 else if (isTie(board, move))
                     macroBoard[macroX][macroY] = "TIE";
 
-                //Check macro win
                 if (isWin(macroBoard, new Move(macroX, macroY), "" + currentPlayer))
                     gameOver = GameOverState.Win;
                 else if (isTie(macroBoard, new Move(macroX, macroY)))
                     gameOver = GameOverState.Tie;
             }
-
         }
 
         private boolean isTie(String[][] board, IMove move) {
-            int localX = move.getX() % 3;
-            int localY = move.getY() % 3;
+            int localX = move.getX() % SUBGRID_SIZE;
+            int localY = move.getY() % SUBGRID_SIZE;
             int startX = move.getX() - (localX);
             int startY = move.getY() - (localY);
 
-            for (int i = startX; i < startX + 3; i++) {
-                for (int k = startY; k < startY + 3; k++) {
+            for (int i = startX; i < startX + SUBGRID_SIZE; i++) {
+                for (int k = startY; k < startY + SUBGRID_SIZE; k++) {
                     if (board[i][k].equals(IField.AVAILABLE_FIELD) ||
                             board[i][k].equals(IField.EMPTY_FIELD))
                         return false;
@@ -201,45 +251,39 @@ public class AiQaeda implements IBot {
             return true;
         }
 
-
         public boolean isWin(String[][] board, IMove move, String currentPlayer) {
-            int localX = move.getX() % 3;
-            int localY = move.getY() % 3;
+            int localX = move.getX() % SUBGRID_SIZE;
+            int localY = move.getY() % SUBGRID_SIZE;
             int startX = move.getX() - (localX);
             int startY = move.getY() - (localY);
 
-            //check col
-            for (int i = startY; i < startY + 3; i++) {
+            for (int i = startY; i < startY + SUBGRID_SIZE; i++) {
                 if (!board[move.getX()][i].equals(currentPlayer))
                     break;
-                if (i == startY + 3 - 1) return true;
+                if (i == startY + SUBGRID_SIZE - 1) return true;
             }
 
-            //check row
-            for (int i = startX; i < startX + 3; i++) {
+            for (int i = startX; i < startX + SUBGRID_SIZE; i++) {
                 if (!board[i][move.getY()].equals(currentPlayer))
                     break;
-                if (i == startX + 3 - 1) return true;
+                if (i == startX + SUBGRID_SIZE - 1) return true;
             }
 
-            //check diagonal
             if (localX == localY) {
-                //we're on a diagonal
                 int y = startY;
-                for (int i = startX; i < startX + 3; i++) {
+                for (int i = startX; i < startX + SUBGRID_SIZE; i++) {
                     if (!board[i][y++].equals(currentPlayer))
                         break;
-                    if (i == startX + 3 - 1) return true;
+                    if (i == startX + SUBGRID_SIZE - 1) return true;
                 }
             }
 
-            //check anti diagonal
-            if (localX + localY == 3 - 1) {
+            if (localX + localY == SUBGRID_SIZE - 1) {
                 int less = 0;
-                for (int i = startX; i < startX + 3; i++) {
+                for (int i = startX; i < startX + SUBGRID_SIZE; i++) {
                     if (!board[i][(startY + 2) - less++].equals(currentPlayer))
                         break;
-                    if (i == startX + 3 - 1) return true;
+                    if (i == startX + SUBGRID_SIZE - 1) return true;
                 }
             }
             return false;
@@ -247,25 +291,14 @@ public class AiQaeda implements IBot {
 
         private void updateMacroboard(IMove move) {
             String[][] macroBoard = currentState.getField().getMacroboard();
-            for (int i = 0; i < macroBoard.length; i++)
-                for (int k = 0; k < macroBoard[i].length; k++) {
-                    if (macroBoard[i][k].equals(IField.AVAILABLE_FIELD))
-                        macroBoard[i][k] = IField.EMPTY_FIELD;
-                }
-
-            int xTrans = move.getX() % 3;
-            int yTrans = move.getY() % 3;
-
-            if (macroBoard[xTrans][yTrans].equals(IField.EMPTY_FIELD))
-                macroBoard[xTrans][yTrans] = IField.AVAILABLE_FIELD;
-            else {
-                // Field is already won, set all fields not won to avail.
-                for (int i = 0; i < macroBoard.length; i++)
-                    for (int k = 0; k < macroBoard[i].length; k++) {
-                        if (macroBoard[i][k].equals(IField.EMPTY_FIELD))
-                            macroBoard[i][k] = IField.AVAILABLE_FIELD;
-                    }
+            for (String[] row : macroBoard) {
+                Arrays.fill(row, IField.AVAILABLE_FIELD);
             }
+
+            int xTrans = move.getX() % SUBGRID_SIZE;
+            int yTrans = move.getY() % SUBGRID_SIZE;
+
+            macroBoard[xTrans][yTrans] = IField.EMPTY_FIELD;
         }
     }
 }
